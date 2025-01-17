7b8277d8b995f3d714572cfbeb68bbf00321e94b
PrimakMV <pm2710@mail.ru>
2025-01-17 22:30:55 +0300

    Added the project structure

---
+++ src/main/java/com/redis_integration_demo/RedisIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.redis_integration_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisIntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redis_integration_demo/RedisIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis_integration_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
