74cdcde76544e7046069e08bf201732aac839de5
PrimakMV <pm2710@mail.ru>
2025-01-18 15:08:49 +0300

    Added a method to the controller for accepting a request from the user

--- src/main/java/com/redis_integration_demo/controller/UserController.java
+++ src/main/java/com/redis_integration_demo/controller/UserController.java
@@ -6,10 +6,7 @@ import com.redis_integration_demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/users")
@@ -22,5 +19,11 @@ public class UserController {
     public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
         UserResponse response = userService.createUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        UserResponse response = userService.getUserById(id);
+        return ResponseEntity.ok(response);
     }
 }
