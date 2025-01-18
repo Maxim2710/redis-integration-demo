a6ab690ee5410f801d6805457c9a4840e7bd8869
PrimakMV <pm2710@mail.ru>
2025-01-18 15:08:22 +0300

    Improved the response model for the user. And also the business logic of obtaining a user by ID is implemented.

--- src/main/java/com/redis_integration_demo/dto/UserResponse.java
+++ src/main/java/com/redis_integration_demo/dto/UserResponse.java
@@ -2,11 +2,13 @@ package com.redis_integration_demo.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
     private Long id;
     private String username;
--- src/main/java/com/redis_integration_demo/service/UserService.java
+++ src/main/java/com/redis_integration_demo/service/UserService.java
@@ -5,7 +5,10 @@ import com.redis_integration_demo.dto.UserResponse;
 import com.redis_integration_demo.model.User;
 import com.redis_integration_demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -28,4 +31,25 @@ public class UserService {
                 user.getCreatedAt().toString()
         );
     }
+
+    // value = "users" -> имя
+    // key = "#id" -> как вычисляется ключ кэша (с помощью id)
+    // unless = "#result == null" -> результат не будет сохранен в кэш, если он null
+    @Cacheable(value = "users", key = "#id", unless = "#result == null")
+    public UserResponse getUserById(Long id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isEmpty()) {
+            throw new RuntimeException("Пользователь не найден");
+        }
+
+        User user = userOptional.get();
+
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getCreatedAt().toString()
+        );
+    }
+
 }
