7dd3357b7ced2b109cb14cc3e9dbf3fcc863445b
PrimakMV <pm2710@mail.ru>
2025-01-18 21:04:45 +0300

    The model for getting a session response has been updated, and methods have also been added to the session repository.

--- src/main/java/com/redis_integration_demo/repository/SessionRepository.java
+++ src/main/java/com/redis_integration_demo/repository/SessionRepository.java
@@ -4,7 +4,11 @@ import com.redis_integration_demo.model.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
     void deleteByUserId(Long userId);
+
+    Optional<Session> findByUserId(Long userId);
 }
