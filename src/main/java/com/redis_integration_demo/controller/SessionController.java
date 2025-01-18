72b9a8dece872cfd57fdf8c077ebfe561df290aa
PrimakMV <pm2710@mail.ru>
2025-01-18 21:06:37 +0300

    Added a method to the controller for processing a session request based on user ID

--- src/main/java/com/redis_integration_demo/controller/SessionController.java
+++ src/main/java/com/redis_integration_demo/controller/SessionController.java
@@ -7,10 +7,7 @@ import com.redis_integration_demo.service.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/sessions")
@@ -26,6 +23,16 @@ public class SessionController {
             return ResponseEntity.ok(response);
         } catch (RuntimeException ex) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(ex.getMessage()));
+        }
+    }
+
+    @GetMapping(path = "/user/{userId}")
+    public ResponseEntity<Object> getSessionByUserId(@PathVariable Long userId) {
+        try {
+            SessionResponse response = sessionService.getSessionByUserId(userId);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getMessage()));
         }
     }
 }
